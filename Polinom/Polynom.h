d22870f0cbe79d48fb1de2908d2f71c39a29d15e
Andrey Morenkov <jedi-andr@mail.ru>
2015-12-14 23:03:39 +0300

    Some updates

---
+++ Polinom/Polynom.h
@@ -0,0 +1,15 @@
+#include "TLink.h"
+
+template <class T>
+class Polynom
+{
+private:
+	TLink<T>* pHead;      // указатель на "голову"
+	TLink<T>* pFirst;     // указатель на первый элемент
+	TLink<T>* pLast;      // указатель на последний элемент
+public:
+	Polynom<T> StrToPol()
+};
+
+
+
